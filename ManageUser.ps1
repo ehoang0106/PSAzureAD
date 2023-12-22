c9c6ab732b7e0597d95b44f8efe1b5929617ed0e
Ethan Hoang <khoang@awakenservices.net>
2023-12-22 12:51:47 -0800

    update some lines of code

--- ManageUser.ps1
+++ ManageUser.ps1
@@ -1,17 +1,17 @@
+# param(
+#         [string]$DisplayName,
+#         [string]$GivenName,
+#         [string]$Surname,
+#         [SecureString]$Password,
+#         [string]$UserPrincipalName,
+#         [string]$Department,
+#         [string]$UsageLocation = "US",
+#         [string]$MailNickName,
+#         [Bool]$AccountEnabled = $true
+#     )
 
-
-function ThemUser {
-    param (
-        [string]$DisplayName,
-        [string]$GivenName,
-        [string]$Surname,
-        [SecureString]$Password,
-        [string]$UserPrincipalName,
-        [string]$Department,
-        [string]$UsageLocation = "US",
-        [string]$MailNickName,
-        [Bool]$AccountEnabled = $true
-    )
+function Adding-NewUser {
+   
 
     # Prompt for user input if not provided as arguments
     if (-not $GivenName){
@@ -40,8 +40,6 @@ function ThemUser {
     }
 
     if(-not $MailNickName){
-        # $MailNickName = Read-Host "Enter Mail Nick Name"
-        # $MailNickName = $MailNickName.replace(' ', '').ToLower()
 
         $MailNickName = $GivenName[0], $Surname -join ""
     }
@@ -50,11 +48,11 @@ function ThemUser {
     $PasswordProfile.Password = $Password
 
     # Create new Azure AD user
-    New-AzureADUser -DisplayName $DisplayName -GivenName $GivenName -Surname $Surname -PasswordProfile $PasswordProfile -UserPrincipalName $UserPrincipalName -Department $Department -UsageLocation $UsageLocation -AccountEnabled $AccountEnabled -MailNickName $MailNickName 
+    New-AzureADUser -DisplayName $DisplayName -GivenName $GivenName -Surname $Surname -PasswordProfile $PasswordProfile -UserPrincipalName $UserPrincipalName -Department $Department -UsageLocation "US" -AccountEnabled $true -MailNickName $MailNickName 
+    Write-Host "$DisplayName has been added!" -ForegroundColor green 
 }
 
 
-ThemUser
 
 # PSAZ-CreateNewUser -Displayname $DisplayName -PasswordProfile $PasswordProfile -UserPrincipalName $UserPrincipalName -UsageLocation $UsageLocation -AccountEnabled $true
 # $LicensedUser = Get-AzureADUser -ObjectId "pngo@awakenservices.net"  
@@ -64,3 +62,47 @@ ThemUser
 # $Licenses = New-Object -TypeName Microsoft.Open.AzureAD.Model.AssignedLicenses 
 # $Licenses.AddLicenses = $License 
 # Set-AzureADUserLicense -ObjectId $User.ObjectId -AssignedLicenses $Licenses
+
+function Removing-User {
+    param (
+        [string] $ObjectID
+    )
+    Write-Host "Warning! This function is removing user. Please be careful!!!" -ForegroundColor Red -BackgroundColor White
+    $ObjectID = Read-Host "Enter user's email"
+
+    $user = Get-AzureADUser -ObjectId $ObjectID
+    $nameOfUser = $user.DisplayName
+
+    Remove-AzureADUser -ObjectId $ObjectID
+
+    Write-Host "$nameOfuser has been removed! The account is still staying at the Deleted User folder" -ForegroundColor Green -BackgroundColor White
+}
+
+function Menu-Option {
+    while($true){
+        Write-Host "`n----------------------"
+        Write-Host "Enter (1) to Add a new user"
+        Write-Host "Enter (2) to Remove an existing user"
+        Write-Host "Enter (3) to Exit"
+        Write-Host "----------------------"
+
+        $option = Read-Host "Enter option"
+
+
+        if ($option -eq "1"){
+            Adding-NewUser
+        }
+        elseif ($option -eq "2"){
+            Removing-User
+        }
+        elseif ($option -eq "3") {
+            break
+        }
+        else {
+            Write-Host "Invalid input. Try again!"
+        }
+    }
+    
+}
+
+Menu-Option
