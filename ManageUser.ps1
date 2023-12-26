bd934c88b18dd105ea225fb26bf7d9b33990859c
Ethan Hoang <khoang@awakenservices.net>
2023-12-26 15:13:49 -0800

    added function to remove m365 license for a user

--- ManageUser.ps1
+++ ManageUser.ps1
@@ -90,49 +90,93 @@ function Grant-365License {
 
 }
 
+
+function Remove-365License {
+    
+    $Email = Read-Host "Enter user's email"
+
+    
+    $User = Get-AzureADUser -ObjectId $Email
+
+    #current assigned licenses of the user
+    $CurrentLicenses = $User.AssignedLicenses
+
+    #create object to hold user's licenses
+    $Licenses = New-Object -TypeName Microsoft.Open.AzureAD.Model.AssignedLicenses
+
+    $LicensesToRemove = "f245ecc8-75af-4f8e-b61f-27d8114de5f3" #this is M365 office license SKU ID
+
+    foreach($li in $CurrentLicenses.SkuId){
+        if($li -eq $LicensesToRemove){
+            $Licenses.RemoveLicenses = $li
+        }
+    }
+    #remove 365 license
+    Set-AzureADUserLicense -ObjectId $User.ObjectId -AssignedLicenses $Licenses
+
+    Write-Host "`nMicrosoft 365 License for $($User.DisplayName)($UserEmail) has been removed!" -ForegroundColor Green 
+    
+}
+
+
+
+
 function Search-365Users {
     $Sku = New-Object -TypeName Microsoft.Open.AzureAD.Model.AssignedLicense
 
     #this is SKU ID of Microsoft 365 Business standard
     $Sku.SkuId = "f245ecc8-75af-4f8e-b61f-27d8114de5f3"
     
-    Get-AzureADUser -All $true | Where-Object {$_.AssignedLicenses.SkuId -contains $Sku.SkuId} | Out-Host
+    Get-AzureADUser -All $true | 
+        Where-Object {$_.AssignedLicenses.SkuId -contains $Sku.SkuId} | 
+        Select-Object DisplayName, UserPrincipalName |  #only show display name and user's email (userprincipalname)
+        Out-Host
 }
+
+
+
+
 function Open-OptionMenu {
     while($true){
-        Write-Host "`n----------------------"
-        Write-Host "Enter (1) to Add a new user"
-        Write-Host "Enter (2) to Remove an existing user"
-        Write-Host "Enter (3) to assigned M365 Office license to a user"
-        Write-Host "Enter (4) to search users that assigned M365 Office license"
-        Write-Host "Enter (q) to Exit"
+        Write-Host "`n----------------------" 
+        Write-Host "     Menu option  " 
+        Write-Host "----------------------" 
+        Write-Host "Enter (1) to Add a new user" -ForegroundColor Green 
+        Write-Host "Enter (2) to Assign M365 Office license to a user" -ForegroundColor Green 
+        Write-Host "Enter (3) to List out users that assigned M365 Office license" -ForegroundColor Green 
+        Write-Host "Enter (4) to Remove an existing user" -ForegroundColor Red 
+        Write-Host "Enter (5) to Unassigned M365 Office license to a user" -ForegroundColor Red 
+        Write-Host "Enter (q) to Exit" -ForegroundColor Red 
         Write-Host "----------------------"
 
-        $option = Read-Host "Enter option"
+        $option = $(Write-Host "Enter option: " -ForegroundColor DarkYellow -NoNewline; Read-Host) 
 
 
         if ($option -eq "1"){
             Enter-NewUser
         }
         elseif ($option -eq "2"){
-            Enter-DelUser
-        }
-        elseif ($option -eq "3") {
             Grant-365License
         }
-        elseif ($option -eq "4") {
+        elseif ($option -eq "3") {
             Search-365Users
+        }
+        elseif ($option -eq "4") {
+            Enter-DelUser
+        }
+        elseif ($option -eq "5") {
+            Remove-365License
         }
         elseif ($option -eq "q"){
             break
         }
         else {
-            Write-Host "Invalid input. Try again!"
+            Write-Host
+            Write-Host "`Invalid input. Try again!" -ForegroundColor Red -BackgroundColor White
         }
     }
     
 }
 
 Open-OptionMenu
-
 
