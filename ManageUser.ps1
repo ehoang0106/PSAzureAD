0f46bae1e9a0f7a5b6f0d4a00a8e8eef7e3b79a8
Khoa Hoang <khoahoang1698@gmail.com>
2024-05-17 12:05:49 -0700

    update reset password function to force user change password next time login

--- ManageUser.ps1
+++ ManageUser.ps1
@@ -99,7 +99,7 @@ function Enter-NewUserPW {
         $User = Get-AzureADUser -ObjectId $Email
         $NameOfUser = $User.DisplayName
 
-        Set-AzureADUserPassword -ObjectId $Email -Password $Password
+        Set-AzureADUserPassword -ObjectId $Email -Password $Password -ForceChangePasswordNextLogin
         Write-Host
         Write-Host "Successfully reset password for $NameOfUser($Email)" -ForegroundColor Green
     }
